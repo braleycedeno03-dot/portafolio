c2e43982ca08046a8ab2f286166650771b6adfa0
braleycedeno03-dot <braleycedeno03@gmail.com>
2026-04-02 12:12:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll(".hidden");
+
+  const mostrar = () => {
+    elements.forEach(el => {
+      const top = el.getBoundingClientRect().top;
+      if (top < window.innerHeight - 50) {
+        el.classList.add("show");
+      }
+    });
+  };
+
+  window.addEventListener("scroll", mostrar);
+  mostrar();
+});
+
+// BOTÓN VOLVER ARRIBA
+const btnTop = document.getElementById("btnTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
+});
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
